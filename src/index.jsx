fdd1dd23e20a1de4833c112503e3bbbdba5fa7de
Ihor Kopieichykov <igor129k02@gmail.com>
2023-03-12 21:55:27 +0200

    initial commit + header

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.css";
+import App from "./components/App/App";
+import Header from "./components/Header/Header";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Header />
+    <App />
+  </React.StrictMode>
+);
