4db78ee9b029ecc17146692d69b2562df0d12b06
Danya <daniilka003@gmail.com>
2023-03-22 21:49:43 +0200

    card of talent

--- src/components/TalentsListPage/TalentsList/TalentsList.jsx
+++ src/components/TalentsListPage/TalentsList/TalentsList.jsx
@@ -1,5 +1,31 @@
 import styles from "./TalentsList.module.scss";
+import { TalentCard } from "./TalentCard/TalentCard";
+import talents from "./shared/data/talents.json";
 
 export function TalentsList() {
-    return <div className={styles["talents-lists"]}></div>;
+    return (
+        <div className={styles["talents-lists"]}>
+            {talents.map(
+                ({
+                    id,
+                    image,
+                    firstname,
+                    lastname,
+                    skills,
+                    specialization,
+                }) => {
+                    return (
+                        <TalentCard
+                            key={id}
+                            image={image}
+                            firstname={firstname}
+                            lastname={lastname}
+                            skills={skills}
+                            specialization={specialization}
+                        />
+                    );
+                }
+            )}
+        </div>
+    );
 }
